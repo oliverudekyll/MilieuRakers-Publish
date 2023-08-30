45c37b7498ffa7bf1517f709073acaeade71f08a
ErikMerisalu <142214016+ErikMerisalu@users.noreply.github.com>
2023-08-30 17:21:46 +0300

    Update landing-page.js

--- landing-page.js
+++ landing-page.js
@@ -1,5 +1,9 @@
 var imagePaths = [
     {
+      preview: "../Images/Content/31/Preview.jpg",
+      hover: ["../Images/Content/31/1.jpg", "../Images/Content/30/2.jpg", "../Images/Content/30/Preview.jpg"]
+    },
+    {
       preview: "../Images/Content/30/Preview.jpg",
       hover: ["../Images/Content/30/1.jpg", "../Images/Content/30/2.jpg", "../Images/Content/30/Preview.jpg"]
     },
